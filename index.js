0c2acf99f6414d4a18b6cd1cb5acad54e59a9e5e
Zainab Saud <zai99nb@gmail.com>
2020-06-11 13:06:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,10 +13,22 @@ const tutorials = [
 
 function titleCased(tutorials){
   return tutorials.map(tutorial => {
-  let tutorial= tutorial.split(" ")})
+  tutorial.split(" ")})
   for (let i=0 ; i<tutorial.length;i++){
     tut = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice
   }
   return tut.join("")
+  }
+  
+  
+  
+  
+  function titleCased(){
+    let t = tutorials.map(x=>{
+      x.split(" ")
+      for (let i=0 ; i<x.length;i++){
+        x[i] = x[i].charAt(0).toUpperCase() + x[i].slice
+      }
+    })
   }
 
