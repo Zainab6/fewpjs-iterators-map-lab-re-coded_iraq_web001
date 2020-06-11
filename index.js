aef3c040a6e4d319521e913587f889879583b4d1
Zainab Saud <zai99nb@gmail.com>
2020-06-11 11:29:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased =tutorials.map ((tuturial) => {
+  return tutorial.charAt(0).toUpperCase()
+})
